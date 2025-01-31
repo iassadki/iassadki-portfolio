fc0a3f399bd02f32990af4b710bf146ef18f05bf
iassadki <ilias.assadki@gmail.com>
2025-01-31 20:55:14 +0100

    push

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/images/react.svg'
-import viteLogo from './assets/images/vite.svg'
-import 'App.css'
+// import reactLogo from './assets/images/react.svg'
+// import viteLogo from './assets/images/vite.svg'
+import './assets/styles/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,10 +10,10 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src="./assets/images/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src="./assets/images/react.svg" className="logo" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
